4ea9a2250a7800a28972908345e7ed3fcef399d7
Sander Botman <sbotman@schubergphilis.com>
2013-10-20 03:09:31 +0200

    adding the baselist and removing the duplicated data / functions

--- lib/chef/knife/github_compare.rb
+++ lib/chef/knife/github_compare.rb
@@ -25,27 +25,13 @@ class Chef
 
       deps do
         require 'chef/knife/github_base'
-
         include Chef::Knife::GithubBase
+        require 'chef/knife/github_baselist'
+        include Chef::Knife::GithubBaseList
       end
       
       banner "knife github compare [COOKBOOK] (options)"
       category "github"
-
-      option :fields,
-             :long => "--fields 'NAME, NAME'",
-             :description => "The fields to output, comma-separated"
-
-      option :noheader,
-             :long => "--noheader",
-             :description => "Removes header from output",
-             :boolean => true
-
-      option :all,
-             :short => "-a",
-             :long => "--all",
-             :description => "Get all cookbooks from github.",
-             :boolean => true
 
       option :mismatch,
              :short => "-m",
@@ -66,6 +52,9 @@ class Chef
         # Get all chef cookbooks and versions.
         cookbooks = rest.get_rest("/cookbooks?num_version=1")
 
+        #Get the github link
+        git_link = get_github_link(nil)
+
         # Filter all repo information based on the tags that we can find
         if config[:fields] || config[:fieldlist]
           all_repos = get_all_repos
@@ -74,19 +63,14 @@ class Chef
           all_repos = {}
           if config[:all]
             get_all_repos.each { |k,v|
-              cookbook = k
               cookbooks[k].nil? || cookbooks[k]['versions'].nil? ? version = "" : version = cookbooks[k]['versions'][0]['version']
-              gh_url = get_github_link(v)
-              gh_tag  = v['latest_tag']
-              all_repos[cookbook] = { 'name' => cookbook, 'latest_cb_tag' => version, 'git_url' => gh_url, 'latest_gh_tag' => gh_tag }
+              all_repos[k] = { 'name' => k, 'latest_cb_tag' => version, 'git_url' => v["#{git_link}"], 'latest_gh_tag' => v['latest_tag'] }
             }
           else
             cookbooks.each { |k,v|
-              cookbook = k
-              version  = v['versions'][0]['version']
-              get_all_repos[k].nil? || get_github_link(get_all_repos[k]).nil? ? gh_url = ui.color("ERROR: Cannot find cookbook!", :red) : gh_url = get_github_link(get_all_repos[k])
+              get_all_repos[k].nil? || get_github_link(get_all_repos[k]).nil? ? gh_url = ui.color("ERROR: Cannot find cookbook!", :red) : gh_url = get_all_repos[k]["#{git_link}"]
               get_all_repos[k].nil? || get_all_repos[k]['latest_tag'].nil? ? gh_tag = ui.color("ERROR: No tags!", :red) : gh_tag = get_all_repos[k]['latest_tag']
-              all_repos[cookbook] = { 'name' => cookbook, 'latest_cb_tag' => version, 'git_url' => gh_url, 'latest_gh_tag' => gh_tag }
+              all_repos[k] = { 'name' => k, 'latest_cb_tag' => v['versions'][0]['version'], 'git_url' => gh_url, 'latest_gh_tag' => gh_tag }
             }
           end
         end
@@ -99,42 +83,16 @@ class Chef
           repos = all_repos 
         end
 
-        # Displaying information based on the fields and repos
-        if config[:fields]
-          object_list = []
-          config[:fields].split(',').each { |n| object_list << ui.color(("#{n}").strip, :bold) }
+        columns = [ 'name,Name', 'latest_cb_tag,Tag', 'git_url,Link', 'latest_gh_tag,Tag' ]
+        match = [ 'latest_cb_tag', 'latest_gh_tag' ]
+
+        if repos.nil? || repos.empty?
+          Chef::Log.error("No repositories found.")
         else
-          object_list = [
-            ui.color('Cookbook', :bold),
-            ui.color('Tag', :bold),
-            ui.color('Github', :bold),
-            ui.color('Tag', :bold)
-          ]
+          display_info(repos, columns, match)
         end
-
-        columns = object_list.count
-        object_list = [] if config[:noheader]
-
-        repos.each do |k,r|
-          if config[:fields]
-             config[:fields].downcase.split(',').each { |n| object_list << ((r[("#{n}").strip]).to_s || 'n/a') }
-          else
-            next if config[:mismatch] && (r['latest_gh_tag'] == r['latest_cb_tag'])
-            r['latest_gh_tag'] == r['latest_cb_tag'] ? color = :white : color = :yellow
-            color = :white if config[:all]
- 
-            object_list << ui.color((r['name'] || 'n/a'), color)
-            object_list << ui.color((r['latest_cb_tag'] || 'n/a'), color)
-            object_list << ui.color((r['git_url'] || 'n/a'), color)
-            object_list << ui.color((r['latest_gh_tag'] || 'n/a'), color)
-          end
-        end
-
-        puts ui.list(object_list, :uneven_columns_across, columns)
 
       end
-    
-
 
     end
   end
